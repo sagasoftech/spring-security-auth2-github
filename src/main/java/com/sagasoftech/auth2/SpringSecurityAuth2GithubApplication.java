7802abf115a6a4e428c93c4ae0b12d95880ab9fd
Sagar Dighe <sagasoftech@gmail.com>
2023-07-16 01:03:19 -0500

    Initial version

---
+++ src/main/java/com/sagasoftech/auth2/SpringSecurityAuth2GithubApplication.java
@@ -0,0 +1,13 @@
+package com.sagasoftech.auth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAuth2GithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAuth2GithubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sagasoftech/auth2/SpringSecurityAuth2GithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sagasoftech.auth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAuth2GithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
